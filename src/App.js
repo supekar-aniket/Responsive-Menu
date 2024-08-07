065225026ae24c38b8a76b7b45b6f5c9328d454f
Aniket Supekar <supekaraniket34@gmail.com>
2024-08-07 13:54:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let [menuStatus,setMenuStatus] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <button className='menuOpenBtn' onClick={()=>{setMenuStatus(!menuStatus)}}> 
+        &#9776;        
+      </button>
+      
+      <div className={`menu ${menuStatus ? 'activeMenu' : ''}`}>
+      <button className='menuCloseBtn' onClick={()=>{setMenuStatus(!menuStatus)}}>
+        &times;
+      </button>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Course</li>
+          <li>Gallary</li>
+          <li>Contact US</li>
+        </ul>
+      </div>
+
     </div>
   );
 }
